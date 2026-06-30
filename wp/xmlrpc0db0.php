98b8b555937be87df731dd2d56d4c9e1589fe827
Jona <jona@offshorly.com>
2026-06-30 09:49:16 +0800

    Fix SEO for production

--- wp/xmlrpc0db0.php
+++ wp/xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://breakmedia.lndo.site</homePageLink>
+		<homePageLink>https://breakmedia.net</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://breakmedia.lndo.site/wp/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://breakmedia.lndo.site/wp/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://breakmedia.lndo.site/wp/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://breakmedia.lndo.site/wp/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://breakmedia.lndo.site/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://breakmedia.net/wp/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://breakmedia.net/wp/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://breakmedia.net/wp/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://breakmedia.net/wp/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://breakmedia.net/wp-json/" />
 			</apis>
 	</service>
 </rsd>
